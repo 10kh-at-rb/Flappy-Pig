976fcd65a815b9bbe39b099f918a877da8ba165c
Karen Ling <karensling@gmail.com>
2015-06-25 06:04:53 -0700

    Implement background bar moving.

--- js/game.js
+++ js/game.js
@@ -12,6 +12,7 @@
     this.score = 0;
     this.gameOver = false;
     this.fired = false;
+    this.interval = 0;
     $(window).keydown(function (e) {
       if (this.fired) {
         this.pig.up();
@@ -29,8 +30,38 @@
     this.pigImage = new Image();
     this.pigImage.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/loading-gif-animate.gif";
 
-    this.bgImage = new Image();
-    this.bgImage.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg.png";
+
+    this.bgImage_1 = new Image();
+    this.bgImage_1.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_1.gif";
+
+    this.bgImage_bar = new Image();
+    this.bgImage_bar.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_bar.gif";
+    // this.bgImage_2 = new Image();
+    // this.bgImage_2.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_2.gif";
+    //
+    // this.bgImage_3 = new Image();
+    // this.bgImage_3.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_3.gif";
+    //
+    // this.bgImage_4 = new Image();
+    // this.bgImage_4.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_4.gif";
+    //
+    // this.bgImage_5 = new Image();
+    // this.bgImage_5.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_5.gif";
+    //
+    // this.bgImage_6 = new Image();
+    // this.bgImage_6.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_6.gif";
+    //
+    // this.bgImage_7 = new Image();
+    // this.bgImage_7.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_7.gif";
+    //
+    // this.bgImage_8 = new Image();
+    // this.bgImage_8.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_8.gif";
+    //
+    // this.bgImage_9 = new Image();
+    // this.bgImage_9.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_9.gif";
+    //
+    // this.bgImage_10 = new Image();
+    // this.bgImage_10.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/bg_10.gif";
 
     this.topPipeImage = new Image();
     this.topPipeImage.src = "https://dl.dropboxusercontent.com/u/2330299/capstone/flappy_pig/pipe_top.png"
@@ -42,7 +73,37 @@
 
   Game.prototype.render = function (ctx) {
     ctx.clearRect(0, 0, this.xDim, this.yDim);
-    // ctx.drawImage(this.bgImage, 0, 0, this.xDim, this.yDim)
+
+    if (this.interval % 1 === 0) {
+      ctx.drawImage(this.bgImage_1, 0, 0, this.xDim, this.yDim);
+    }
+    // } else if (this.interval % 9 === 0) {
+    //   ctx.drawImage(this.bgImage_9, 0, 0, this.xDim, this.yDim)
+    // } else if (this.interval % 8 === 0) {
+    //   ctx.drawImage(this.bgImage_8, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 7 === 0) {
+    //   ctx.drawImage(this.bgImage_7, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 6 === 0) {
+    //   ctx.drawImage(this.bgImage_6, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 5 === 0) {
+    //   ctx.drawImage(this.bgImage_5, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 4 === 0) {
+    //   ctx.drawImage(this.bgImage_4, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 3 === 0) {
+    //   ctx.drawImage(this.bgImage_3, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 2 === 0) {
+    //   ctx.drawImage(this.bgImage_2, 0, 0, this.xDim, this.yDim);
+    // } else if (this.interval % 1 === 0) {
+    //   ctx.drawImage(this.bgImage_1, 0, 0, this.xDim, this.yDim);
+    // }
+    // ctx.drawImage(this.bgImage_1, 0, 0, this.xDim, this.yDim)
+
+    console.log(this.interval);
+    console.log(this.xDim);
+    // if (this.interval % this.xDim === 0) {
+      ctx.drawImage(this.bgImage_bar, (this.obstacles[0].fromLeft - this.xDim), 0, 2250, 28.5);
+    // }
+
     ctx.drawImage(this.pigImage, this.pig.left, this.pig.top, this.pig.width, this.pig.height);
 
     this.obstacles.forEach(function (obstacle) {
@@ -110,9 +171,10 @@
     var ctx = this.canvas.getContext("2d");
 
     window.setInterval((function () {
+      this.interval += 1;
       this.movePig();
       this.moveObstacles();
       this.render(ctx);
-    }).bind(this), 1000/60);
+    }).bind(this), 1000/200);
   };
 })();
