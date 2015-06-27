4e0379ff85a7c4e6f8ca5f840ae4ff3c7a482243
Karen Ling <karensling@gmail.com>
2015-06-26 22:41:20 -0700

    Custom error message for length of name.

--- flappy_pig/app/models/users_score.rb
+++ flappy_pig/app/models/users_score.rb
@@ -1,4 +1,4 @@
 class UsersScore < ActiveRecord::Base
   validates :name, :score, presence: true;
-  validates :name, length: { maximum: 12 }
+  validates :name, length: { maximum: 12, message: "must be < 13 letters" }
 end
